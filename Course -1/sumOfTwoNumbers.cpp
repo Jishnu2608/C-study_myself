3eb17de2c6a342f8218d3f0c59983e8eae4d3014
Jishnu2608 <jishnudeepborah@gmail.com>
2021-07-18 22:58:51 +0530

    first program and theory

---
+++ Course -1/sumOfTwoNumbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a,b,c;
+    cout<<"Enter 2 numbers: ";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"Sum of a and b is: "<<c;
+    return 0;
+}
+
+//to get string as a whole, use getline(cin,...);
