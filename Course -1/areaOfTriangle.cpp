94c872e88bc73d196cca091cf86a8a3b312b5f75
Jishnu2608 <jishnudeepborah@gmail.com>
2021-07-19 23:07:08 +0530

    going on

---
+++ Course -1/areaOfTriangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float base,height,area;
+    cout<<"Enter base and height"<<endl;
+    cin>>base>>height;
+    area=(base*height)/2;
+    cout<<"Area is: "<<area<<endl;
+    return 0;
+}
